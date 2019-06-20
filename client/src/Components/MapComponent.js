a863ab8550fca1e1d83c24706826b6b43f943608
Daniel Murphy <rdanielmurphy@gmail.com>
2019-06-19 20:25:24 -0400

    Added apparatus data

--- client/src/Components/MapComponent.js
+++ client/src/Components/MapComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './MapComponent.css';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import IncidentsService from '../Services/IncidentsService';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import EnrichService from '../Services/EnrichService';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function keyToTitle(key) {
     return key.toLowerCase()
@@ -28,6 +33,38 @@ const KeyValueDisplay = ({ data }) =>
         )
     });
 
+const ApparatusStatusDisplay = ({ data }) =>
+    Object.entries(data.unit_status).map(([k, v]) => {
+        return <Marker color="red" position={[v.latitude, v.longitude]}>
+            <Popup>
+                {data.unit_type + " " + data.unit_id}
+                <br />
+                {k + " at " + v.timestamp}
+            </Popup>
+        </Marker>
+    });
+
+const ApparatusRouteDisplay = ({ data }) => {
+    let positions = [];
+    let statuses = [];
+
+    Object.entries(data.unit_status).map(([k, v]) => {
+        statuses.push(v);
+    });
+
+    statuses.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return new Date(b.timestamp) - new Date(a.timestamp);
+    });
+
+    statuses.map((v) => {
+        positions.push([v.latitude, v.longitude]);
+    });
+
+    return <Polyline positions={positions} />;
+}
+
 class MapComponent extends Component {
     constructor(props) {
         super(props);
@@ -38,7 +75,8 @@ class MapComponent extends Component {
             zoom: 10,
             incident: null,
             loaded: false,
-            weather: {}
+            weather: {},
+            apparatus: undefined
         }
     }
 
@@ -53,13 +91,27 @@ class MapComponent extends Component {
                     zoom: 16,
                     incident: incident,
                     loaded: true,
-                    weather: weather
+                    weather: weather,
+                    apparatus: undefined
                 });
             });
         });
     }
 
+    handleListItemClick(e, obj) {
+        if (this.state.apparatus && this.state.apparatus.car_id === obj.car_id) {
+            this.setState({
+                apparatus: null
+            });
+        } else {
+            this.setState({
+                apparatus: obj
+            });
+        }
+    }
+
     render() {
+        const scope = this;
         if (!this.state.loaded) {
             return (
                 <div className="loading">
@@ -84,6 +136,20 @@ class MapComponent extends Component {
                                 {this.state.incident.address.city + ", " + this.state.incident.address.state}
                             </Popup>
                         </Marker>
+                        {(() => {
+                            if (this.state.apparatus) {
+                                return (
+                                    <ApparatusStatusDisplay data={this.state.apparatus} />
+                                )
+                            }
+                        })()}
+                        {(() => {
+                            if (this.state.apparatus) {
+                                return (
+                                    <ApparatusRouteDisplay data={this.state.apparatus} />
+                                )
+                            }
+                        })()}
                     </Map>
                     <Paper className="details">
                         <h3>Description</h3>
@@ -110,6 +176,26 @@ class MapComponent extends Component {
                                 <KeyValueDisplay data={this.state.incident.fire_department} />
                             </tbody>
                         </table>
+                        <h3>Apparatuses (Click to see route)</h3>
+                        <List component="nav" aria-label="Main mailbox folders">
+                            {this.state.incident.apparatus.map((object, i) => {
+                                return (
+                                    <ListItem
+                                        button
+                                        onClick={event => scope.handleListItemClick(event, object)}>
+                                        <ListItemIcon>
+                                            <Checkbox
+                                                edge="start"
+                                                checked={this.state.apparatus && this.state.apparatus.car_id === object.car_id ? true : false}
+                                                tabIndex={-1}
+                                                disableRipple
+                                            />
+                                        </ListItemIcon>
+                                        <ListItemText primary={object.unit_type + " " + object.unit_id} />
+                                    </ListItem>
+                                );
+                            })}
+                        </List>
                     </Paper>
                 </div>
             );
